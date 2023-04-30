c9c9a919c79b82a3acfbf60102bdba8973fcc5a7
Serega <antonovseantonoff@yandex.ru>
2023-04-30 21:38:39 +0300

    Made it easy to add photos

--- places/admin.py
+++ places/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
+from django.contrib.admin import TabularInline
 from .models import Place, Image
+
+
+class ImageAdminInline(TabularInline):
+    extra = 1
+    model = Image
 
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
+    inlines = (ImageAdminInline,)
     list_display = ['title']
 
 
