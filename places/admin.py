16a61a3b3c6fbd851d07409657f5b7d5ae2f70a4
Serega <antonovseantonoff@yandex.ru>
2023-05-02 11:04:57 +0300

    Added preview

--- places/admin.py
+++ places/admin.py
@@ -1,11 +1,21 @@
 from django.contrib import admin
 from django.contrib.admin import TabularInline
+from django.utils.html import format_html
 from .models import Place, Image
 
 
 class ImageAdminInline(TabularInline):
     extra = 1
     model = Image
+    readonly_fields = ['headshot_image']
+    fields = ('img', 'headshot_image',)
+
+    def headshot_image(self, obj):
+        return format_html(
+            '<img src="{url}" style="max-height: 200px;"/>'.format(
+                url=obj.img.url,
+            )
+        )
 
 
 @admin.register(Place)
