71d5020824db9b3596e77f31b993425c3839fc3d
Serega <antonovseantonoff@yandex.ru>
2023-05-02 15:28:50 +0300

    Connected WYSIWYG editor

--- places/models.py
+++ places/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from tinymce.models import HTMLField
 
 
 class Place(models.Model):
     title = models.CharField('Название места', max_length=200)
     description_short = models.TextField('Короткое описание')
-    description_long = models.TextField('Длинное описание')
+    description_long = HTMLField('Длинное описание')
     lng = models.FloatField('Долгота', null=True)
     lat = models.FloatField('Широта', null=True)
 
@@ -22,7 +23,8 @@ class Image(models.Model):
         verbose_name='Относится к месту',
         related_name='images',
         on_delete=models.SET_NULL,
-        null=True)
+        null=True
+    )
     img = models.ImageField('Картинка', upload_to='', null=True, blank=True)
     my_order = models.PositiveIntegerField('Порядок', default=1)
 
