db5e882de9ac30005110476551389c6330162cfc
Serega <antonovseantonoff@yandex.ru>
2023-05-03 13:20:41 +0300

    Added address

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env.str('SECRET_KEY', 'REPLACE_ME')
 DEBUG = env.bool('DEBUG', True)
 
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1'])
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', '.pythonanywhere.com'])
 
 
 # Application definition
