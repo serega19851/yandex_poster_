11405079074ae4668b68fff032beb2c436466533
Serega <antonovseantonoff@yandex.ru>
2023-05-06 10:48:27 +0300

    Fixed file

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -1,60 +1,70 @@
-import os
-import sys
-from io import BytesIO
-from urllib.parse import urlsplit
+from hashlib import md5
+
 from django.core.management.base import BaseCommand
+from django.core.files.base import ContentFile
 import requests
 from places.models import Place, Image
 
 
-def get_imagetitle(link):
-    parsed_link = urlsplit(link)
-    return os.path.split(parsed_link.path)[1]
-
-
 class Command(BaseCommand):
-    help = ''' 
-    Загружает данные в БД по переданной ссылке,
-    ссылка должна содержать json-файл.
-    '''
+    help = '''Загружает данные в БД по переданной ссылке, \
+              ссылка должна содержать json-файл.'''
 
     def add_arguments(self, parser):
         parser.add_argument('link', type=str, help='Ссылка на json-файл.')
+
+    def download_place_images(self, place, images_links):
+        for image_link in images_links:
+            try:
+                response = requests.get(image_link)
+                response.raise_for_status()
+
+                content_img = ContentFile(
+                    response.content,
+                    name=md5(response.content).hexdigest(),
+                )
+                Image.objects.create(place=place, img=content_img)
+            except requests.exceptions.HTTPError as http_er:
+                self.stderr.write(self.style.ERROR(
+                    f'\n Ошибка загрузки изображения\n{http_er}\n\n'
+                ))
+                continue
 
     def handle(self, *args, **options):
         link = options.get('link')
         try:
             response = requests.get(link)
             response.raise_for_status()
-            data_json = response.json()
-            images_links = data_json.get('imgs')
-            place, new_place = Place.objects.get_or_create(
-                title=data_json.get('title'),
+            payload = response.json()
+            images_links = payload.get('imgs', [])
+
+            place, created = Place.objects.get_or_create(
+                title=payload['title'],
                 defaults={
-                    'description_short': data_json.get('description_short'),
-                    'description_long': data_json.get('description_long'),
-                    'lng': data_json.get('coordinates').get('lng'),
-                    'lat': data_json.get('coordinates').get('lat'),
+                    'description_short': payload.get('description_short', ''),
+                    'description_long': payload.get('description_long', ''),
+                    'lng': payload.get('coordinates')['lng'],
+                    'lat': payload.get('coordinates')['lat'],
                 }
             )
-            place = place if place else new_place
-        except requests.exceptions.HTTPError as http_er:
-            sys.stderr.write(f'\n Ошибка загрузки json-файла.\n{http_er}\n\n')
-            return
-        for image_link in images_links:
-            try:
-                imagetitle = get_imagetitle(image_link)
-                response = requests.get(image_link)
-                response.raise_for_status()
-                content = response.content
-                image, new_image = Image.objects.get_or_create(
-                    place=place,
-                    imagetitle=imagetitle,
+            if not created:
+                self.stdout.write(
+                    self.style.WARNING(f'\n{place.title} уже есть в БД.\n')
                 )
-                if new_image:
-                    image.img.save(imagetitle, BytesIO(content), save=True)
-            except requests.exceptions.HTTPError as http_er:
-                sys.stderr.write(
-                    f'\n Ошибка загрузки изображения\n{http_er}\n\n')
-                continue
-        sys.stdout.write(f'\n{place.title} добавлено в БД.\n')
+                return
+
+        except requests.exceptions.HTTPError as http_error:
+            self.stderr.write(self.style.ERROR(
+                f'\n Ошибка загрузки json-файла.\n{http_error}\n\n'
+            ))
+            return
+        except KeyError as key_error:
+            self.stderr.write(self.style.ERROR(
+                f'\n Отсутствует обязательный ключ\n{key_error}\n\n'
+            ))
+            return
+
+        self.download_place_images(place, images_links)
+        self.stdout.write(
+            self.style.SUCCESS(f'\n{place.title} добавлено в БД.\n')
+        )
